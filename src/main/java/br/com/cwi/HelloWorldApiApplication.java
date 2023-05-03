9ba7e16a568804a188422623cb8449c3b5a817e0
Arthur Kist Juchem <arthur.juchem@cwi.com.br>
2023-05-03 09:30:24 -0300

    initial commit

---
+++ src/main/java/br/com/cwi/HelloWorldApiApplication.java
@@ -0,0 +1,20 @@
+package br.com.cwi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HelloWorldApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldApiApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String hello() {
+        return "Olá Mundo!";
+    }
+}
